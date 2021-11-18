f0f1bb283dcbd32209b78c7c7a59c768351fa25c
Kostich31 <k.v.kostinich@mail.ru>
2021-11-18 19:43:24 +0300

    tests

--- CloudSky_server/tests/tests.cpp
+++ CloudSky_server/tests/tests.cpp
@@ -2,8 +2,19 @@
 #include "../include/SoundComponent.h"
 #include "../include/VideoComponent.h"
 
+TEST(video, incorrectInit){
+  VideoComponent video;
+  ASSERT_EQ(video.initVideoComponent(), false);
+}
+
+TEST(audio, incorrectInit){
+  SoundComponent audio;
+  ASSERT_EQ(audio.initSoundComponent(), false);
+}
+
+
 int main(int argc, char **argv) {
+
   ::testing::InitGoogleTest(&argc, argv);
-  
   return RUN_ALL_TESTS();
 }
