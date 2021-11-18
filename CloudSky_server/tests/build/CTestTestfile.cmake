54a087d4bf02c4126004d3a14ae10a65997a09a8
Kostich31 <k.v.kostinich@mail.ru>
2021-11-18 17:46:41 +0300

    delete cmake files

--- CloudSky_server/tests/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/konstantin/cloud/CloudSky/CloudSky_server/tests
-# Build directory: /home/konstantin/cloud/CloudSky/CloudSky_server/tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
